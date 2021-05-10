4b67a160efe7b80dd29bc873408674c74d4a9a51
Cesar Felipe Lopez <>
2021-05-10 22:46:01 +0000

    Hip Hop Wiki now has albums from other years!

--- website/models.py
+++ website/models.py
@@ -7,6 +7,6 @@ class Album(db.Model):
   artist = db.Column(db.String(150))
   name =  db.Column(db.String(150))
   record_label = db.Column(db.String(150))
-  
+  year = db.Column(db.Integer)
   
   
--- website/views.py
+++ website/views.py
@@ -10,16 +10,30 @@ import itertools
 
 views = Blueprint('views', __name__)
 
+#Write to text file
 def writefile(filename, txt):
     with open(filename, 'w') as writer:
       writer.write(str(txt))
 
+#Function that gets rows inside of the table
+def get_table_rows(the_table):
+  albums =[]
+  table_rows = the_table.find_all('tr')
+  for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text for i in td] #list comprehension
+    albums.append(row)
+
+  return albums
+
+#Remove square brackets from wiki sources
 def source_cleanup(txt):
   brack1 = txt.find('[')
   txt = txt[0:brack1-1]
 
   return txt
 
+#Recursive function that changes dates with 0 value 
 def fix_dates(album):
   if album.day != 0:
     return album.day
@@ -27,6 +41,13 @@ def fix_dates(album):
     album_id = album.id
     prev_album = Album.query.filter_by(id=album_id-1).first()
     return fix_dates(prev_album)
+
+#Remove text from title to get year
+def title_cleanup(txt):
+  title = txt.find(' in hip hop music')
+  txt = txt[0:title]
+
+  return txt
 
 #home page
 @views.route('/')
@@ -40,8 +61,8 @@ def home():
 @views.route('/fill_db')
 def fill_db():
   all_albums = Album.query.all()
-  print(all_albums)
-  print(type(all_albums))
+  #print(all_albums)
+  #print(type(all_albums))
 
   if not all_albums:
 
@@ -49,29 +70,15 @@ def fill_db():
 
     soup = BeautifulSoup(source, 'lxml')
 
-    textfile_name = 'albums.txt'
+    
 
     # get text where div has id== bodyContent
     body_content = soup.find(id='bodyContent')
 
     mytables = soup.find_all('table', {'class':'wikitable'})
-
+    # textfile_name = 'albums.txt'
     # writefile(textfile_name, mytables[0])
 
-
-    #-------------
-    #Function that gets rows inside of the table
-    def get_table_rows(the_table):
-      albums =[]
-      table_rows = the_table.find_all('tr')
-      for tr in table_rows:
-        td = tr.find_all('td')
-        row = [i.text for i in td] #list comprehension
-        albums.append(row)
-
-      return albums
-
- 
     #####################################################
     # parsing through list of tables and adding ablums to db
     for a_table in itertools.islice(mytables , 0, 8):
@@ -89,14 +96,17 @@ def fill_db():
             album_name = a_album[1] #instead of a_album[2]
             record_label = a_album[2] #instead of a_album[3]
             record_label = source_cleanup(record_label)
+            year = soup.find(id="firstHeading").string
+            year = title_cleanup(year)
+            # print(year)
 
-            new_album = Album(month=month_str, day=day, artist=artist, name=album_name, record_label=record_label)
+            new_album = Album(month=month_str, day=day, artist=artist, name=album_name, record_label=record_label, year=year)
             db.session.add(new_album)
             db.session.commit()
 
           else:
             #is the correct size so should have correct collumns. so has day published
-            print(a_album)
+            # print(a_album)
 
             month_str = month.span.string
             day = a_album[0]
@@ -104,8 +114,10 @@ def fill_db():
             album_name = a_album[2]
             record_label = a_album[3]
             record_label = source_cleanup(record_label)
+            year = soup.find(id="firstHeading").string
+            year = title_cleanup(year)
 
-            new_album = Album(month=month_str, day=day, artist=artist, name=album_name, record_label=record_label)
+            new_album = Album(month=month_str, day=day, artist=artist, name=album_name, record_label=record_label, year=year)
             db.session.add(new_album)
             db.session.commit()
 
@@ -138,3 +150,165 @@ def wiki():
 def about():
   return render_template('about.html')
 
+#List all page
+@views.route('/list')
+def list_all():
+  all_albums = Album.query
+
+  return render_template('list_all.html', album_list=all_albums)
+
+@views.route('/list/<int:year>')
+def list_year(year):
+  album_year = Album.query.filter_by(year=year)
+  
+  return render_template('list.html', album_list=album_year, year=year)
+
+
+@views.route('/update/<int:year>')
+def update_year(year):
+  album_year = Album.query.filter_by(year=year).all()
+  #print(album_year)
+  year = int(year)
+    
+  if not album_year:
+    source_txt = 'https://en.wikipedia.org/wiki/'+str(year)+'_in_hip_hop_music'
+    source = requests.get(source_txt).text
+
+    soup = BeautifulSoup(source, 'lxml')
+    mytables = soup.find_all('table', {'class':'wikitable'})
+    # print('URL:',source_txt)
+
+    #Table format not the same for list before 2016 &2012
+    if year < 2017:
+      #redirected to be parsed through page differently. Only one table with diff dates
+      return redirect('/update/v2/'+ str(year))
+    #this is the main parser that finds all tables that are organized by months
+    elif year < 2022:
+      #used for looping through table
+      last_table_loc = 0
+      if year == 2021:
+        last_table_loc = 10 #2021 only up to sept (FOR NOW!!!) must limit total table parsed through
+      else: 
+        last_table_loc = 12
+      # parsing through list of tables and adding ablums to db
+      for a_table in itertools.islice(mytables , 0, last_table_loc):
+        month = a_table.find_previous_sibling('h3')
+        tables_albums = get_table_rows(a_table)
+        #parsing through albums in the table
+        for a_album in tables_albums:
+          # print(len(a_album))
+          #checking collumn numbers
+          if len(a_album)>0:
+            if len(a_album)<=2:
+              for test_col in a_album:
+                print('----------test--------------',test_col)
+            elif len(a_album)==4 or len(a_album)==3:
+              #does not have day published bc len==4
+              #len==3 bc busdriver in 2018 only has 3 td
+              day = 0  #*** just set automatically to 0
+              month_str = month.span.string
+              artist = a_album[0] #instead of a_album[1]
+              album_name = a_album[1] #instead of a_album[2]
+              record_label = a_album[2] #instead of a_album[3]
+              record_label = source_cleanup(record_label)
+              year = soup.find(id="firstHeading").string
+              year = title_cleanup(year)
+              # print('Test: ', album_name)
+
+              new_album = Album(month=month_str, day=day, artist=artist, name=album_name, record_label=record_label, year=year)
+              db.session.add(new_album)
+              db.session.commit()
+
+            else:
+              #is the correct size so should have correct collumns. so has day published
+              # print(a_album)
+
+              month_str = month.span.string
+              day = a_album[0]
+              artist = a_album[1]
+              album_name = a_album[2]
+              record_label = a_album[3]
+              record_label = source_cleanup(record_label)
+              year = soup.find(id="firstHeading").string
+              year = title_cleanup(year)
+
+              new_album = Album(month=month_str, day=day, artist=artist, name=album_name, record_label=record_label, year=year)
+              db.session.add(new_album)
+              db.session.commit()
+
+  print("Sucessfully Updated DB")
+  
+  #trying to fix dates
+  album_year = Album.query.filter_by(year=year)
+  for albums in album_year:
+    albums.day = fix_dates(albums)
+  db.session.commit()
+  album_year = Album.query.filter_by(year=year)
+
+  return render_template('list.html', album_list=album_year, year=year)
+
+@views.route('/delete/<int:year>')
+def delete_year(year):
+  albums_year = Album.query.filter_by(year=year).delete()
+  # all_albums = Album.query.delete()
+  db.session.commit()
+  albums_year = Album.query.filter_by(year=year)
+  return render_template('list.html', album_list=albums_year, year=year)
+
+@views.route('/update/v2/<int:year>')
+def update_year2(year):
+  album_year = Album.query.filter_by(year=year).all()
+  #print(album_year)
+  year = int(year)
+    
+  if not album_year:
+    source_txt = 'https://en.wikipedia.org/wiki/'+str(year)+'_in_hip_hop_music'
+    source = requests.get(source_txt).text
+
+    soup = BeautifulSoup(source, 'lxml')
+
+    main_table = soup.find('table', {'class':'wikitable'})
+
+    tables_albums = get_table_rows(main_table)
+        #parsing through albums in the table
+    for a_album in tables_albums:
+      print('Album length:',len(a_album))
+      if len(a_album)==0:
+        pass
+      elif len(a_album)<3:
+        pass
+      elif len(a_album)==4 or len(a_album)==3:
+        date = 0
+        month = date
+        day = date
+        artist = a_album[0] #instead of a_album[1]
+        album_name = a_album[1] #instead of a_album[2]
+        
+        year = soup.find(id="firstHeading").string
+        year = title_cleanup(year)
+        # print('Test: ', album_name)
+
+        new_album = Album(month=month, day=day, artist=artist, name=album_name, record_label=0, year=year)
+        db.session.add(new_album)
+        db.session.commit()
+      else:
+        #is the correct size so should have correct collumns. so has day published
+        # print(a_album)
+        date = a_album[0]
+        date_split = date.split(" ")
+        month = date_split[0]
+        day = date_split[1]
+        artist = a_album[1]
+        album_name = a_album[2]
+        year = soup.find(id="firstHeading").string
+        year = title_cleanup(year)
+
+        new_album = Album(month=month, day=day, artist=artist, name=album_name, record_label=0, year=year)
+        db.session.add(new_album)
+        db.session.commit()
+
+  print("Sucessfully Updated DB")
+  
+  album_year = Album.query.filter_by(year=year)
+
+  return render_template('list.html', album_list=album_year, year=year)
